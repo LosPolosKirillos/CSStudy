0aba5a64f8e6d38df75cc4084f219901e78a1f36
Кирилл Лиховидов <cosmolihov3@gmail.com>
2024-09-15 10:29:46 +0300

    password prog

--- CSStudy/Program.cs
+++ CSStudy/Program.cs
@@ -11,47 +11,23 @@ namespace CSStudy
     {
         static void Main(string[] args)
         {
-            string userAnswer;
-            bool askQuestion;
-            Console.WriteLine("Масса обьекта m = 2 г, тогда сила тяжести," +
-                " которая действует на объект Ft = ?:");
-            Console.WriteLine("- a) 20Н");
-            Console.WriteLine("- b) 0.02Н");
-            Console.WriteLine("- c) 2Н");
-            Console.WriteLine("- d) 0.002Н");
-            Console.WriteLine("Образец ответа: a)");
+            int triesCount = 5;
+            string password = "1234";
+            string userInput;
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < triesCount; i++)
             {
-                Console.WriteLine("");
-            }
-
-            askQuestion = true;
-            userAnswer = "";
-            while (askQuestion)
-            {
-                Console.Write("Ввод:");
-                userAnswer = Console.ReadLine();
-
-                if (userAnswer != "a)" && userAnswer != "b)" && userAnswer != "c)" && userAnswer != "d)")
+                Console.Write("Введите неверный пароль:");
+                userInput = Console.ReadLine();
+                if (userInput != password)
                 {
-                    continue;
+                    Console.WriteLine("Мужиик");
+                    break;
                 }
                 else
                 {
-                    switch (userAnswer)
-                    {
-                        case "a)":
-                        case "c)":
-                        case "d)":
-                        default:
-                            Console.WriteLine("Ответ неверный!");
-                            break;
-                        case "b)":
-                            Console.WriteLine("Ответ правильный!");
-                            askQuestion = false;
-                            break;
-                    }
+                    Console.WriteLine("Введён верный пароль!");
+                    Console.WriteLine("У вас осталось попыток: " + (triesCount - i - 1) + ".");
                 }
             }
         }
