4fae1737f85f911acfe2a2c8ee8f3d829d768ce7
Кирилл Лиховидов <cosmolihov3@gmail.com>
2024-09-21 12:55:10 +0300

    gladiator's battle

--- CSStudy/Program.cs
+++ CSStudy/Program.cs
@@ -12,37 +12,50 @@ namespace CSStudy
     {
         static void Main(string[] args)
         {
-            int number;
-            int lower, higher;
-            int triesCount = 5;
-            int userInput;
-            Random rand = new Random();
+            Random random = new Random();
+            float health1 = random.Next(90, 110);
+            int damage1 = random.Next(7, 35);
+            int armor1 = random.Next(45, 100);
 
-            number = rand.Next(0, 100);
-            lower = rand.Next(number - 10, number);
-            higher = rand.Next(number + 1, number + 10);
+            float health2 = random.Next(80, 120);
+            int damage2 = random.Next(5, 40);
+            int armor2 = random.Next(65, 100);
 
-            Console.WriteLine($"Я загадал число от 0 до 100, оно больше чем {lower}, " +
-                $"но меньше чем {higher}.");
-            Console.WriteLine($"Что это за число? Кол-во попыток: {triesCount}.");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(0, 1);
+            Console.WriteLine($"Гладиатор 1 - {health1} здоровья, {damage1} наносимый урон, {armor1} броня.");
+            Console.SetCursorPosition(0, 3);
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine($"Гладиатор 2 - {health2} здоровья, {damage2} наносимый урон, {armor2} броня.");
 
-            while (triesCount-- > 0)
+            Console.SetCursorPosition(0, 5);
+            Console.ForegroundColor = ConsoleColor.Black;
+            while (health1 > 0 && health2 > 0)
             {
-                Console.Write("Твой ответ: ");
-                userInput = Convert.ToInt32(Console.ReadLine());
-                if (userInput == number)
-                {
-                    Console.WriteLine($"Вы правы, это было число {number}.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Неверно, попробуйте еще раз.");
-                }
+                health1 -= Convert.ToSingle(random.Next(0, damage2 + 1)) / 100 * armor1;
+                health2 -= Convert.ToSingle(random.Next(0, damage1 + 1)) / 100 * armor2;
+
+                Console.WriteLine($"Здоровье гладиатора 1: {health1}");
+                Console.WriteLine($"Здоровье гладиатора 2: {health2}");
             }
-            if (triesCount < 0)
+
+            if (health1 <= 0 && health2 <= 0)
             {
-                Console.WriteLine($"Вы проиграли, увы, но повезёт в следующий раз. Это было число {number}.");
+                Console.WriteLine("Ничья, \nоба гладиатора погибли.");
+            }
+            else if (health1 <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Гладиатор 1\n - пал.");
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            else if (health2 <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Гладиатор 2\n - пал.");
+                Console.ForegroundColor = ConsoleColor.Black;
             }
         }
     }
