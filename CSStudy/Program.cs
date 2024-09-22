da39c830f0a5b33311c04f53752509c63708ea3a
Кирилл Лиховидов <cosmolihov3@gmail.com>
2024-09-22 21:17:47 +0300

    library

--- CSStudy/Program.cs
+++ CSStudy/Program.cs
@@ -12,24 +12,82 @@ namespace CSStudy
     {
         static void Main(string[] args)
         {
-            int[,] array = new int[15, 2];
-            int[,] array1 = new int[2, 2]
+            bool isOpen = true;
+            string[,] books =
             {
-                { 9, 2 },
-                { 2, 2 },
+                {"Александр Пушкин", "Михаил Лермонтов", "Сергей Есенин" },
+                {"Роберт Мартин", "Джесси Шелл", "Сергей Тепляков" },
+                {"Стивен Кинг", "Говард Лавкрафт", "Брем Стокер" }
             };
 
-            Random random = new Random();
-
-            for (int i = 0; i < array1.GetLength(0); i++)
+            while (isOpen)
             {
-                for (int j = 0; j < array1.GetLength(1); j++)
+                Console.SetCursorPosition(0, 20);
+                Console.WriteLine("\nВесь список авторов:\n");
+                for (int i = 0; i < books.GetLength(0); i++)
                 {
-                    array1[i, j] = random.Next(0, 10);
-                    Console.Write(array1[i, j]);
+                    for (int j = 0; j < books.GetLength(1); j++)
+                    {
+                        Console.Write(books[i, j] + " | ");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
-                Console.WriteLine(array[0, 0]);
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Библиотека");
+                Console.WriteLine("\n1 - узнать имя автора по индексу книги." +
+                    "\n\n2 - найти книгу по автору.\n\n3 - выход.");
+                Console.Write("\nВыберите пункт меню: ");
+
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        int line, column;
+                        Console.Write("Введите номер полки: ");
+                        line = Convert.ToInt32(Console.ReadLine());
+                        line--;
+                        Console.Write("Введите номер столбца: ");
+                        column = Convert.ToInt32(Console.ReadLine());
+                        column--;
+                        Console.WriteLine("Это автор: " + books[line, column]);
+                        break;
+                    case 2:
+                        string author;
+                        bool authorIsFound = false;
+                        Console.Write("Введите автора: ");
+                        author = Console.ReadLine();
+                        for (int i = 0; i < books.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < books.GetLength(1); j++)
+                            {
+                                if (author.ToLower() == books[i, j].ToLower())
+                                {
+                                    Console.Write($"Автор {books[i, j]} находится по адресу полка " +
+                                        $"{i + 1}, место {j + 1}.");
+                                    authorIsFound = true;
+                                }
+                            }
+                        }
+                        if (authorIsFound == false)
+                        {
+                            Console.WriteLine("Такого автора нет.");
+                        }
+                        break;
+                    case 3:
+                        isOpen = false;
+                        break;
+                    default:
+                        Console.WriteLine("Введена неверная команда.");
+                        break;
+                }
+
+                if (isOpen)
+                {
+                    Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
             }
         }
     }
