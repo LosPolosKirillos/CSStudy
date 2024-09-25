68e6df9c61895aa754ee33b1e8a7345018e3d5da
Кирилл Лиховидов <cosmolihov3@gmail.com>
2024-09-25 17:25:10 +0300

    tictactoe

--- CSStudy/Program.cs
+++ CSStudy/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,82 +13,120 @@ namespace CSStudy
     {
         static void Main(string[] args)
         {
-            bool isOpen = true;
-            string[,] books =
-            {
-                {"Александр Пушкин", "Михаил Лермонтов", "Сергей Есенин" },
-                {"Роберт Мартин", "Джесси Шелл", "Сергей Тепляков" },
-                {"Стивен Кинг", "Говард Лавкрафт", "Брем Стокер" }
+            Random random = new Random();
+            string[,] cells = new string[3, 3] {
+                { "-", "-", "-" },
+                { "-", "-", "-" },
+                { "-", "-", "-" }
             };
+            int line = -1, column = -1;
+            bool isGamePlaying = true;
 
-            while (isOpen)
+            while (isGamePlaying)
             {
-                Console.SetCursorPosition(0, 20);
-                Console.WriteLine("\nВесь список авторов:\n");
-                for (int i = 0; i < books.GetLength(0); i++)
+                for (int i = 0; i < cells.GetLength(0); i++)
                 {
-                    for (int j = 0; j < books.GetLength(1); j++)
+                    for (int j = 0; j < cells.GetLength(1); j++)
                     {
-                        Console.Write(books[i, j] + " | ");
+                        if (j < cells.GetLength(1) - 1)
+                        {
+                            Console.Write(cells[i, j] + " | ");
+                        }
+                        else
+                        {
+                            Console.Write(cells[i, j] + "\n");
+                        }
                     }
-                    Console.WriteLine();
+                    if (i < cells.GetLength(0) - 1)
+                    {
+                        Console.WriteLine("--  --  --");
+                    }
                 }
 
-                Console.SetCursorPosition(0, 0);
-                Console.WriteLine("Библиотека");
-                Console.WriteLine("\n1 - узнать имя автора по индексу книги." +
-                    "\n\n2 - найти книгу по автору.\n\n3 - выход.");
-                Console.Write("\nВыберите пункт меню: ");
+                line = -1;
+                column = -1;
 
-                switch (Convert.ToInt32(Console.ReadLine()))
+                while ((line != 0 && line != 1 && line != 2) || (column != 0 && column != 1 && column != 2))
                 {
-                    case 1:
-                        int line, column;
-                        Console.Write("Введите номер полки: ");
-                        line = Convert.ToInt32(Console.ReadLine());
-                        line--;
-                        Console.Write("Введите номер столбца: ");
-                        column = Convert.ToInt32(Console.ReadLine());
-                        column--;
-                        Console.WriteLine("Это автор: " + books[line, column]);
-                        break;
-                    case 2:
-                        string author;
-                        bool authorIsFound = false;
-                        Console.Write("Введите автора: ");
-                        author = Console.ReadLine();
-                        for (int i = 0; i < books.GetLength(0); i++)
-                        {
-                            for (int j = 0; j < books.GetLength(1); j++)
-                            {
-                                if (author.ToLower() == books[i, j].ToLower())
-                                {
-                                    Console.Write($"Автор {books[i, j]} находится по адресу полка " +
-                                        $"{i + 1}, место {j + 1}.");
-                                    authorIsFound = true;
-                                }
-                            }
-                        }
-                        if (authorIsFound == false)
-                        {
-                            Console.WriteLine("Такого автора нет.");
-                        }
-                        break;
-                    case 3:
-                        isOpen = false;
-                        break;
-                    default:
-                        Console.WriteLine("Введена неверная команда.");
-                        break;
-                }
+                    Console.Write("Введите номер строки: ");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            line = 0;
+                            break;
+                        case "2":
+                            line = 1;
+                            break;
+                        case "3":
+                            line = 2;
+                            break;
+                        default:
+                            Console.WriteLine("Неверный индекс.");
+                            break;
 
-                if (isOpen)
+                    }
+                    Console.Write("Введите номер столбца: ");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            column = 0;
+                            break;
+                        case "2":
+                            column = 1;
+                            break;
+                        case "3":
+                            column = 2;
+                            break;
+                        default:
+                            Console.WriteLine("Неверный индекс.");
+                            break;
+                    }
+                }
+                if (cells[line, column] == "-")
                 {
-                    Console.WriteLine("\nНажмите любую клавишу для продолжения...");
-                }
+                    cells[line, column] = "x";
 
-                Console.ReadKey();
-                Console.Clear();
+                    if (cells[0,0] == "x" && cells[0, 1] == "x" && cells[0, 2] == "x") { isGamePlaying = false; break; }
+                    else if (cells[1, 0] == "x" && cells[1, 1] == "x" && cells[1, 2] == "x") { isGamePlaying = false; break; }
+                    else if (cells[2, 0] == "x" && cells[2, 1] == "x" && cells[2, 2] == "x") { isGamePlaying = false; break; }
+                    else if (cells[0, 0] == "x" && cells[1, 0] == "x" && cells[2, 0] == "x") { isGamePlaying = false; break; }
+                    else if (cells[0, 1] == "x" && cells[1, 1] == "x" && cells[2, 1] == "x") { isGamePlaying = false; break; }
+                    else if (cells[0, 2] == "x" && cells[1, 2] == "x" && cells[2, 2] == "x") { isGamePlaying = false; break; }
+                    else if (cells[0, 0] == "x" && cells[1, 1] == "x" && cells[2, 2] == "x") { isGamePlaying = false; break; }
+                    else if (cells[0, 2] == "x" && cells[1, 1] == "x" && cells[2, 0] == "x") { isGamePlaying = false; break; }
+
+                    while (true)
+                    {
+                        line = random.Next(0, 3);
+                        column = random.Next(0, 3);
+                        if (cells[line, column] == "-")
+                        {
+                            cells[line, column] = "o";
+                            break;
+                        }
+                    }
+                    if (cells[0, 0] == "o" && cells[0, 1] == "o" && cells[0, 2] == "o") { isGamePlaying = false; break; }
+                    else if (cells[1, 0] == "o" && cells[1, 1] == "o" && cells[1, 2] == "o") { isGamePlaying = false; break; }
+                    else if (cells[2, 0] == "o" && cells[2, 1] == "o" && cells[2, 2] == "o") { isGamePlaying = false; break; }
+                    else if (cells[0, 0] == "o" && cells[1, 0] == "o" && cells[2, 0] == "o") { isGamePlaying = false; break; }
+                    else if (cells[0, 1] == "o" && cells[1, 1] == "o" && cells[2, 1] == "o") { isGamePlaying = false; break; }
+                    else if (cells[0, 2] == "o" && cells[1, 2] == "o" && cells[2, 2] == "o") { isGamePlaying = false; break; }
+                    else if (cells[0, 0] == "o" && cells[1, 1] == "o" && cells[2, 2] == "o") { isGamePlaying = false; break; }
+                    else if (cells[0, 2] == "o" && cells[1, 1] == "o" && cells[2, 0] == "o") { isGamePlaying = false; break; }
+                }
+                else
+                {
+                    Console.WriteLine("Эта ячейка занята!");
+                }
+            }
+            
+            if (cells[line, column] == "x")
+            {
+                Console.WriteLine("Победа игрока!");
+            }
+            else
+            {
+                Console.WriteLine("Победа рандома!");
             }
         }
     }
