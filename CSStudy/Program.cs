3419222002569f78905f42d2a2c7751e0a42fdcf
Кирилл Лиховидов <cosmolihov3@gmail.com>
2024-10-06 23:10:58 +0300

    overload

--- CSStudy/Program.cs
+++ CSStudy/Program.cs
@@ -15,16 +15,41 @@ namespace CSStudy
     {
         static void Main(string[] args)
         {
-            int[] group = { 1, 2, 3 };
-
-            group = minusValue(group, 1, 4);
-            Console.WriteLine(group[1]);
+            int[] array1 = new int[5];
+            int[,] array2 = new int[5, 5];
+            array1 = Resize(array1, 6);
+            array2 = Resize(array2, 10, 10);
+            Console.WriteLine(array1.Length);
+            Console.WriteLine(array2.Length);
         }
 
-        static int[] minusValue(int[] list, int index, int value = 2)
+        static int[] Resize(int[] array, int length)
         {
-            list[index] -= value;
-            return list;
+            int[] tempArray = new int[length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+
+            array = tempArray;
+            return array;
+        }
+
+        static int[,] Resize(int[,] array, int x, int y)
+        {
+            int[,] tempArray = new int[x, y];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    tempArray[i, j] = array[i, j];
+                }
+            }
+
+            array = tempArray;
+            return array;
         }
     }
 }
